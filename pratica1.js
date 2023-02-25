d1330004dc0fc65d7d73c73f82a927d41ba07e52
Rebeca Beatriz de Luna <rebecabeatrizdeluna@gmail.com>
2023-02-24 23:44:29 -0300

    Atividades práticas das aulas.

--- pratica1.js
+++ pratica1.js
@@ -24,3 +24,22 @@ console.log(eDaAmericaLatina)
 
 // Usando a concatenação para unir texto e variáveis
 console.log("Eu me chamo Rebeca Beatriz e moro no " + nomeDoPais +", que tem " + capitalDoPais + " como Capital. Aqui, sou muito feliz pois tenho " + quantidadeDeEstados + " Estados para desbravar!")
+
+// 3) Crie as seguintes constantes e em seguida use a criatividade para concatená-las, formando uma frase: primeiro nome; último nome; ano de nascimento; cor preferida; hobby; um sentimento.
+
+const primeiroNome = "Rebeca Beatriz";
+const ultimoNome = "de Luna";
+const anoDeNascmento = 2000;
+const corPreferida = "azul";
+const hobby = "cantar";
+const sentimento = "felicidade";
+
+console.log(primeiroNome)
+console.log(ultimoNome)
+console.log(anoDeNascmento)
+console.log(corPreferida)
+console.log(hobby)
+console.log(sentimento)
+
+console.log("Olá, me chamo " + primeiroNome, ultimoNome + ", nasci no dia 15 de Agosto de " + anoDeNascmento + ". Eu amo a cor " + corPreferida + " e meu hobby favorito é " + hobby + ", isso me trasmite muita " + sentimento + ". Foi um prazer apresentar um pouco sobre mim.")
+
