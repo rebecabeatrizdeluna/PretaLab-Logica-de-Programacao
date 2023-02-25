b44630c9f72099901b80f490c38cd6c0c80501c5
Rebeca Beatriz de Luna <rebecabeatrizdeluna@gmail.com>
2023-02-25 15:37:01 -0300

    Prática das aulas 2

---
+++ pratica2.js
@@ -0,0 +1,38 @@
+// 1) Crie duas constantes, número 1 e número 2, onde cada uma deve conter um valor numérico. Em seguida, crie mais cinco constantes, soma, subtração, multiplicação, divisão e módulo, que contenham as respectivas operações usando as constantes número 1 e número 2. 
+
+const numero1 = 15;
+const numero2 = 2;
+
+const soma = numero1 + numero2;
+const subtração = numero1 - numero2;
+const multiplicação = numero1 * numero2;
+const divisão = numero1 / numero2;
+const modulo = numero1 % numero2;
+
+console.log("A soma é: " + soma)
+console.log("A subtração é: " + subtração)
+console.log("A multiplicação é: " + multiplicação)
+console.log("A divisão é: " + divisão)
+console.log("O módulo é: " + modulo)
+
+// Aprendendo o uso das condicionais: if, if else
+let hora = 10 
+if (hora < 12){
+    console.log("Bom dia!");
+} else {
+    console.log("Boa tarde!");
+}
+
+// 2) Declare um valor numérico e analise se ele é maior ou não do que 10. Se sim, imprima na tela: "Esse número é maior que 10". Se não for, imprima: "Esse número é menor que 10".
+
+let numero = 22
+if (numero > 10){
+    console.log("Esse número é maior que 10.");
+} else {
+    console.log("Esse número é menor que 10.");
+}
+
+
+
+
+
