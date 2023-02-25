2a6c0727ba2d61125a711f3a3374cc3dc8edd161
Rebeca Beatriz de Luna <rebecabeatrizdeluna@gmail.com>
2023-02-24 22:21:35 -0300

    Atividades práticas das aulas

---
+++ pratica1.js
@@ -0,0 +1,26 @@
+// 1) Crie 3 constantes e mostre no console o valor salvo nelas
+
+const fruta1 = "banana";
+const fruta2 = "maçã";
+const fruta3 = "lranja";
+
+console.log(fruta1)
+console.log(fruta2)
+console.log(fruta3)
+
+// 2) Declare e insira os valores nas seguintes variáveis ou constantes: Nome do país, Capital do país, Pontos turísticos, Quantidade de Estados, O país é da América Latina?
+
+const nomeDoPais = "Brasil";
+const capitalDoPais = "Brasília";
+const pontosTuristicos = "Cristo Redentor, Pelourinho, Marco Zero";
+const quantidadeDeEstados = 26;
+const eDaAmericaLatina = true;
+
+console.log (nomeDoPais)
+console.log(capitalDoPais)
+console.log(pontosTuristicos)
+console.log(quantidadeDeEstados)
+console.log(eDaAmericaLatina)
+
+// Usando a concatenação para unir texto e variáveis
+console.log("Eu me chamo Rebeca Beatriz e moro no " + nomeDoPais +", que tem " + capitalDoPais + " como Capital. Aqui, sou muito feliz pois tenho " + quantidadeDeEstados + " Estados para desbravar!")
